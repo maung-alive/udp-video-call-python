7e9b6b6fddfc9a42048e461dcd3a096de6225653
Maung <maung.alive@gmail.com>
2024-02-24 09:50:36 +0630

    Coded server

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import socket
+import cv2
+import pickle
+import numpy
 
-# Press the green button in the gutter to run the script.
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-import socket,cv2,pickle,numpy
+RHOST = "192.168.100.156"   # Change your own
+RPORT = 6001        # Change your own
+
+s.bind((RHOST, RPORT))
+
+while True:
+    (addr, data) = s.recvfrom(65535)
+    print(f"Connected from : {addr[0]}:{addr[1]}")
+
+    data = pickle.loads(data)
+    data = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    cv2.imshow('my pic', data)
+    if cv2.waitKey(10) == 13:
+        break
+
+cv2.destroyAllWindows()        # Close all windows
 
 if __name__ == '__main__':
     print('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
