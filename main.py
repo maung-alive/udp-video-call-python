de21d00d12c5fe60d86a80321994226c17e2f477
Maung <maung.alive@gmail.com>
2024-02-24 09:43:46 +0630

    Installing packages

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import socket
+
+# Press the green button in the gutter to run the script.
+
+import socket,cv2,pickle,numpy
+
+if __name__ == '__main__':
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
