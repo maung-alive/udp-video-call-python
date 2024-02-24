c90e6db91dd70afa1ad0ba8d882464810ced3f48
Maung <maung.alive@gmail.com>
2024-02-24 14:27:13 +0630

    Fixed BUGS

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
 import socket
 import cv2
-import pickle
 import numpy
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 65535)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10000000)
 
 RHOST = '127.0.0.1'
 RPORT = 6001
@@ -13,14 +12,15 @@ capture = cv2.VideoCapture(0)
 
 def receive():
     while True:
-        addr, data = s.recvfrom(65535)
-        data = pickle.loads(data)
-        data = cv2.imdecode(data, cv2.IMREAD_COLOR)
+        (data, addr) = s.recvfrom(65507)
+        data = cv2.imdecode(numpy.frombuffer(data, dtype=numpy.uint8), 1)
+
         cv2.imshow(f"SERVER: {RHOST}", data)
 
         if cv2.waitKey(10) == 13:  # ENTER key to quit
             break
 
 if __name__ == '__main__':
-    s.sendto(b'CONNECTED', (RHOST, RPORT))
+    s.sendto(b'TESTING', (RHOST, RPORT))
+    print("CONNECTED TO SERVER")
     receive()
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import socket
 import cv2
-import pickle
-import numpy
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10000000)
 
-LHOST = "192.168.100.156"   # Change your own
+LHOST = "127.0.0.1"   # Change your own
 LPORT = 6001        # Change your own
 
 s.bind((LHOST, LPORT))
@@ -15,21 +14,22 @@ def transmit(addr):
     while True:
         ret, photo = capture.read()
 
-        ret, buffer = cv2.imencode(".jpg", photo, [int(cv2.IMWRITE_JPEG_QUALITY),30])
-        data = cv2.imdecode(data, cv2.IMREAD_COLOR)
-        bytes = pickle.dumps(data)
-        cv2.imshow(f"Client {addr[0]}", buffer) # Show opencv window
-        s.sendto(bytes, addr)
+
+        photo = cv2.resize(photo, (400,400))
+        encoded_frame = cv2.imencode('.jpg', photo)[1].tobytes()
+
+        s.sendto(encoded_frame, addr)
         if cv2.waitKey(10) == 13:   # Enter Key then quit
             break
 
 def main():
     while True:
-        (addr, data) = s.recvfrom(65535)
+        print(f"Listening Incoming connections...")
+        (data, addr) = s.recvfrom(65507)
         print(f"Connected from : {addr[0]}:{addr[1]}")  # Logging client
         transmit(addr)
 
 
 if __name__ == '__main__':
-    print('PyCharm')
+    print(f"Running server at {LHOST}:{LPORT}")
     main()
