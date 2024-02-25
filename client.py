0cf206fd1d77a2eba88ccc06566b7b7e43f63f9a
Maung <maung.alive@gmail.com>
2024-02-25 14:33:11 +0630

    Done! but too slow and error all the time

--- client.py
+++ client.py
@@ -1,14 +1,18 @@
 import socket
 import cv2
 import numpy
+import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10000000)
 
 RHOST = '127.0.0.1'
 RPORT = 6001
+width, height = 320, 240
 
 capture = cv2.VideoCapture(0)
+capture.set(3, width)
+capture.set(4, height)
 
 def receive():
     while True:
@@ -20,7 +24,17 @@ def receive():
         if cv2.waitKey(10) == 13:  # ENTER key to quit
             break
 
+def transmit(addr):
+    while True:
+        ret, photo = capture.read()
+        encoded_frame = cv2.imencode('.jpg', photo)[1].tobytes()
+        s.sendto(encoded_frame, addr)
+
 if __name__ == '__main__':
     s.sendto(b'TESTING', (RHOST, RPORT))
     print("CONNECTED TO SERVER")
-    receive()
+    addr = (RHOST, RPORT)
+    transmit_thread = threading.Thread(target=transmit, args=(addr,))  # Threading
+    receive_thread = threading.Thread(target=receive)  # Threading
+    transmit_thread.start()
+    receive_thread.start()
--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 import socket
 import cv2
+import threading
+import numpy
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10000000)
 
 LHOST = "127.0.0.1"   # Change your own
 LPORT = 6001        # Change your own
+width, height = 320, 240
 
 s.bind((LHOST, LPORT))
 
 capture = cv2.VideoCapture(0)   # Change to your own webcam or video file
+capture.set(3, width)
+capture.set(4, height)
 def transmit(addr):
     while True:
         ret, photo = capture.read()
-
-
-        photo = cv2.resize(photo, (400,400))
         encoded_frame = cv2.imencode('.jpg', photo)[1].tobytes()
 
         s.sendto(encoded_frame, addr)
-        if cv2.waitKey(10) == 13:   # Enter Key then quit
+
+def receive():
+    while True:
+        (data, addr) = s.recvfrom(65507)
+        data = cv2.imdecode(numpy.frombuffer(data, dtype=numpy.uint8), 1)
+
+        cv2.imshow(f"CLIENT: {addr[0]}:{addr[1]}", data)
+        if cv2.waitKey(10) == 13:
             break
 
 def main():
     while True:
         print(f"Listening Incoming connections...")
         (data, addr) = s.recvfrom(65507)
-        print(f"Connected from : {addr[0]}:{addr[1]}")  # Logging client
-        transmit(addr)
+        print(f"Connected from : {addr[0]}:{addr[1]}")  # Logging client on Terminal
+
+        transmit_thread = threading.Thread(target=transmit, args=(addr,))   # Threading
+        receive_thread = threading.Thread(target=receive)   # Threading
+        # Start Threads
+        transmit_thread.start()
+        receive_thread.start()
 
 
 if __name__ == '__main__':
